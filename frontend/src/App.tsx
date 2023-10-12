987669b00d39b434d155a87cf6007b6f1034d15b
Anil <anildevlpr@gmail.com>
2023-10-12 20:50:45 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <p className='mt-4 ml-4 flex justify-center text-xx'>hello</p>
+    </>
+  )
+}
+
+export default App
